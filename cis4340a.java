606844f5640c494b4ba3dbac26fd02b72d1db4ae
linahhosney <li373447@ucf.edu>
2026-03-08 23:07:18 -0400

    Add files via upload

---
+++ cis4340a.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac Cis4615a.java
+ *  Execution:    java Cis4615a
+ *
+ *  Prints 
+ *    "Hello, World - from the Lina Hosney".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java Cis4615a
+ *  Hello, World - from the class of CIS4615 - Fall 2022 - UCF
+ *  %
+ *
+ ******************************************************************************/
+
+public class cis4340a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from Lina Hosney");
+    }
+
+}
